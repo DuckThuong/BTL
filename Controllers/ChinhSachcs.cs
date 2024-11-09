8ad6a5c88498086d3274703df9eb2b7410e0980c
DuckThuong <trinhthuong26022003@gmail.com>
2024-11-09 12:41:22 +0700

    feat: detail

---
+++ Controllers/ChinhSachcs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL.Controllers
+{
+    public class ChinhSachcs : Controller
+    {
+        public IActionResult ChinhSach()
+        {
+            return View();
+        }
+    }
+}
