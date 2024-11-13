3ee843e0a74b1c2621f5e5bdda3479ee50897635
thuongtrinhduc <thuongtrinhduc@tsoft.vn>
2024-11-13 14:36:41 +0700

    fix code

--- obj/Debug/net8.0/BTL.AssemblyInfo.cs
+++ obj/Debug/net8.0/BTL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b71be11e55c903accbb4290cc605dc101f8a07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+859f56a64f15359560d3e5b1cda614badd7dbf1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
